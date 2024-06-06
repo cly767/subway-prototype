e7c1a2fc4f675ded52006d40578c3bd8b7d3f498
cly767 <chenliangyu01@126.com>
2024-06-06 22:47:00 +0800

    navigation: structure & animation

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,41 @@
-let navigationEnter = document.getElementById('navigationEnter');
 let navigation= document.getElementById('navigation');
+let navigationToggle = document.getElementById('navigationToggle');
+let siteLinks = document.getElementById('siteLinks');
 
-function enterNavigation() {
-	navigation.classList = 'activated';
+function showSiteLinks(linkList) {
+	let i = 0;
+	let timerid = setInterval(() => {
+		linkList[i].classList.remove('prepared');
+		if(i >= linkList.length -1) {
+			clearInterval(timerid);
+			return;
+		}
+		i++;
+	}, 50);
 }
 
-navigationEnter.addEventListener('click', e => {
-	enterNavigation();
-});
+function prepareSiteLinks(linkList) {
+	for( i of linkList )
+		i.classList.add('prepared');
+}
+
+function enterNavigation(e) {
+	navigation.classList.add('activated');
+	navigationToggle.innerText = navigationToggle.dataset.active;
+	navigationToggle.removeEventListener('click', enterNavigation);
+	navigationToggle.addEventListener('click', leaveNavigation);
+	setTimeout(showSiteLinks, 100, siteLinks.children);
+}
+
+function leaveNavigation(e) {
+	prepareSiteLinks(siteLinks.children);
+	setTimeout(() => {
+		navigation.classList.remove('activated');
+		navigationToggle.innerText = navigationToggle.dataset.inactive;
+		navigationToggle.removeEventListener('click', leaveNavigation);
+		navigationToggle.addEventListener('click', enterNavigation);
+	}, 270);
+}
+
+navigationToggle.addEventListener('click', enterNavigation);
+navigationToggle.innerText = navigationToggle.dataset.inactive;
