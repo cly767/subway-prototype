7e75f3d8c0493fd461afd851bb3f3a7829241251
cly767 <chenliangyu01@126.com>
2024-06-13 00:16:49 +0800

    transition & comments

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,5 @@
 'use strict';
+// getting stuff
 let navigation= document.getElementById('navigation');
 let navigationToggle = document.getElementById('navigationToggle');
 let siteLinks = document.getElementById('siteLinks');
@@ -7,8 +8,10 @@ let timerid;
 
 function showSiteLinks(linkList) {
 	let i = 0;
+	// one by one
 	timerid = setInterval(() => {
 		linkList[i].classList.remove('prepared');
+		// we've reached the end
 		if(i >= linkList.length -1) {
 			clearInterval(timerid);
 			return;
@@ -23,22 +26,41 @@ function prepareSiteLinks(linkList) {
 }
 
 function enterNavigation(e) {
-	navigation.classList.add('activated');
-	navigationToggle.classList.add('activated');
+	// re-register event listeners
 	navigationToggle.removeEventListener('click', enterNavigation);
 	navigationToggle.addEventListener('click', leaveNavigation);
-	setTimeout(showSiteLinks, 100, siteLinks.children);
+	// activate the button
+	navigationToggle.classList.add('activated');
+	// fade out
+	contents.classList.add('transparent')
+	setTimeout(linkList => {
+		// activate the navigation layer
+		navigation.classList.add('activated');
+		// hide the contents
+		contents.classList.add('truncated');
+		showSiteLinks(linkList);
+	}, 100, siteLinks.children);
 }
 
 function leaveNavigation(e) {
+	// we're leaving, so stop entering
 	clearInterval(timerid);
+	// restore the link list
 	prepareSiteLinks(siteLinks.children);
+	// deactivate the button
 	navigationToggle.classList.remove('activated');
+	// deactivate the navigation layer
+	navigation.classList.remove('activated');
+	// wait for the links to disappear
 	setTimeout(() => {
-		navigation.classList.remove('activated');
+		// show & fade in
+		contents.classList.remove('truncated');
+		contents.classList.remove('transparent');
+		// re-register event listeners
 		navigationToggle.removeEventListener('click', leaveNavigation);
 		navigationToggle.addEventListener('click', enterNavigation);
 	}, 270);
 }
 
+// register event listener
 navigationToggle.addEventListener('click', enterNavigation);
