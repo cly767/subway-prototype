66b17d62375b845dd3560f8e06b8f8bcc38ef404
cly767 <chenliangyu01@126.com>
2024-06-16 00:09:42 +0800

    easy code copying

--- TODO.md
+++ TODO.md
@@ -1,10 +1,10 @@
-- [ ] a11y: screen readers, reduced motion
-- [ ] easy code copying
 - [ ] font
 - [ ] compatibility
+- [ ] a11y: screen readers, reduced motion
 - [ ] credit: hexo-theme-oranges
 
 ---
 
 - [X] overlay navigation
 - [X] index vertical scroll
+- [X] easy code copying
---
+++ js/copyCode.js
@@ -0,0 +1,26 @@
+'use strict';
+let codeList = document.getElementsByTagName('figure');
+for( let i of codeList ) {
+	let codeTable = i.childNodes[0];
+	let codeHeader = document.createElement('div');
+	let codeLang = document.createElement('div');
+	let separator = document.createElement('div');
+	let copyCode = document.createElement('button');
+
+	codeHeader.classList.add('codeHeader');
+
+	let lang = i.classList[1];
+	if( lang !== undefined )
+		codeLang.innerText = lang;
+
+	separator.classList.add('separator');
+
+	copyCode.innerText = 'COPY';
+	copyCode.classList.add('outlined');
+	copyCode.addEventListener('click', e => {
+		navigator.clipboard.writeText(codeTable.getElementsByClassName('code')[0].innerText);
+	});
+
+	codeHeader.append(codeLang, separator, copyCode);
+	i.insertBefore(codeHeader, codeTable);
+}
