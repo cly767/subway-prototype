8d56150e6f4b1acf0cac84e8523b1c46349954be
cly767 <chenliangyu01@126.com>
2024-06-19 16:40:01 +0800

    wrap js in functions

--- js/copyCode.js
+++ js/copyCode.js
@@ -1,26 +1,30 @@
 'use strict';
-let codeList = document.getElementsByTagName('figure');
-for( let i of codeList ) {
-	let codeTable = i.childNodes[0];
-	let codeHeader = document.createElement('div');
-	let codeLang = document.createElement('div');
-	let separator = document.createElement('div');
-	let copyCode = document.createElement('button');
+function setupCopyCode() {
+	let codeList = document.getElementsByTagName('figure');
+	for( let i of codeList ) {
+		let codeTable = i.childNodes[0];
+		let codeHeader = document.createElement('div');
+		let codeLang = document.createElement('div');
+		let separator = document.createElement('div');
+		let copyCode = document.createElement('button');
 
-	codeHeader.classList.add('codeHeader');
+		codeHeader.classList.add('codeHeader');
 
-	let lang = i.classList[1];
-	if( lang !== undefined )
-		codeLang.innerText = lang;
+		let lang = i.classList[1];
+		if( lang !== undefined )
+			codeLang.innerText = lang;
 
-	separator.classList.add('separator');
+		separator.classList.add('separator');
 
-	copyCode.innerText = 'COPY';
-	copyCode.classList.add('outlined');
-	copyCode.addEventListener('click', e => {
-		navigator.clipboard.writeText(codeTable.getElementsByClassName('code')[0].innerText);
-	});
+		copyCode.innerText = 'COPY';
+		copyCode.classList.add('outlined');
+		copyCode.addEventListener('click', e => {
+			navigator.clipboard.writeText(codeTable.getElementsByClassName('code')[0].innerText);
+		});
 
-	codeHeader.append(codeLang, separator, copyCode);
-	i.insertBefore(codeHeader, codeTable);
+		codeHeader.append(codeLang, separator, copyCode);
+		i.insertBefore(codeHeader, codeTable);
+	}
 }
+
+setupCopyCode();
